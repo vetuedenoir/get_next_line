048a9817215887dfcc04a2684aa1d0eda2cc992f
vetuedenoir <kscordel@e2r13p12.clusters.42paris.fr>
2023-01-16 16:35:18 +0100

    opti min

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,14 +6,14 @@
 /*   By: kscordel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 17:34:15 by kscordel          #+#    #+#             */
-/*   Updated: 2023/01/09 18:30:08 by kscordel         ###   ########.fr       */
+/*   Updated: 2023/01/16 16:33:01 by kscordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 100
+#  define BUFFER_SIZE 1000
 # endif
 
 # include <stdlib.h>
@@ -22,9 +22,9 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-size_t	ft_testreturn(const char *s);
+size_t	ft_testreturn(const char *s, char f);
 char	*ft_read_file(char *stash, int fd);
-char	*ft_add_buf(char *stash, char *buf, size_t index);
+char	*ft_add_buf(char *stash, char *buf, size_t total);
 char	*ft_copy(char *stash, size_t *t);
 char	*ft_clean(char *stash, size_t t);
 char	*movechar(char *str, size_t index);
